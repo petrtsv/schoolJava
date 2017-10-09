81148e884973475b84e5a53ddc072bc3375ae1c0
Petr Tsvetkov <petr.tsv@gmail.com>
2017-10-09 16:44:06 +0300

    Tasks 8 and 9 done

--- src/main/java/com/epam/javacourses2016/task8/SolverTask8.java
+++ src/main/java/com/epam/javacourses2016/task8/SolverTask8.java
@@ -1,5 +1,7 @@
 package com.epam.javacourses2016.task8;
 
+import java.util.Stack;
+
 /**
  * Задана строка, возможно содержащая символы '(', ')', '[', ']', '{', '}'.
  * Проверить правильность расстановки скобок.
@@ -15,7 +17,34 @@ public class SolverTask8 {
      * @param string Анализируемая строка.
      * @return true - скобки расставлены верно, иначе - false.
      */
-    public boolean isNormalBrackets(String string) {
-        return false;
+    public boolean isNormalBrackets(String string)
+    {
+        Stack<Character> stack = new Stack<Character>();
+        String open = "([{";
+        String close = ")]}";
+        boolean result = true;
+        for (char chr : string.toCharArray())
+        {
+            if (open.indexOf(chr) != -1)
+            {
+                stack.add(chr);
+            } else if (close.indexOf(chr) != -1)
+            {
+                if (stack.empty() || open.indexOf(stack.lastElement()) != close.indexOf(chr))
+                {
+                    result = false;
+                }
+                if (!stack.empty())
+                {
+                    stack.pop();
+                }
+            }
+        }
+        if (stack.size() > 0)
+        {
+            result = false;
+        }
+
+        return result;
     }
 }
--- src/main/java/com/epam/javacourses2016/task9/SolverTask9.java
+++ src/main/java/com/epam/javacourses2016/task9/SolverTask9.java
@@ -1,7 +1,9 @@
 package com.epam.javacourses2016.task9;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.HashSet;
+import java.util.Scanner;
 
 /**
  * Задан файл, содержащий английские слова (без знаков препинания).
@@ -9,14 +11,24 @@ import java.util.HashSet;
  * Слова, отличающиеся только регистром букв, считать одинаковыми.
  * Использовать класс HashSet.
  */
-public class SolverTask9 {
+public class SolverTask9
+{
 
     /**
      * Формирует множество уникальных слов, извлекая их из файла.
+     *
      * @param input Анализируемый файл.
      * @return Множество полученных слов.
      */
-    public HashSet<String> getUniqueWords(File input) {
-        return null;
+    public HashSet<String> getUniqueWords(File input) throws FileNotFoundException
+    {
+        HashSet<String> result = new HashSet<>();
+        Scanner scanner = new Scanner(input);
+        while (scanner.hasNext())
+        {
+            String word = scanner.next().toLowerCase();
+            result.add(word);
+        }
+        return result;
     }
 }
